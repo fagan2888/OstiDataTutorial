90d312e84ece9e3b07c2027cfb50d8e5f8ad488f
keknight <knight.kathryn@gmail.com>
2016-08-29 16:03:17 -0400

    updated readme, file

--- Python/README.md
+++ Python/README.md
@@ -2,4 +2,10 @@
 
 The scripts provided in this portion of the tutorial were created by a non-developer (me) as a means of quickly working with large datasets and remediating a significant number of MARC records. 
 
-ostinos.py, coupled with ostinos.csv, is an example of how you might quickly work through a list of known OSTI numbers to derive additional or updated information about a group of reports. 
+##What are these for?
+
+These short Python scripts are simple examples of how you might use OSTI's XML service to either enhance current bibliographic records or create new records. 
+
+`ostinos.py`, coupled with the short dataset `ostinos.csv`, is an example of how you might quickly work through a list of known OSTI numbers to derive additional or updated information about a group of reports. 
+
+`osti_doi.py`, coupled with the much larger dataset `osti_dois.csv`, is an example of using a simple web scraper to grab the dois for a series of known OSTI numbers. 
--- Python/ostinos.py
+++ Python/ostinos.py
@@ -1,24 +1,13 @@
 import requests
 from xml.etree import ElementTree
+from lxml import etree
 import csv
+import pymarc
+from pymarc import Record, Field
 
-f = open('ostinos.csv')
-csv_f = csv.reader(f)
 
-out = open('osti_recs.csv', 'w')
-data = csv.writer(out)
-data.writerow(['Title', 'Author', 'Date', 'Subjects', 'Description', 'OstiID', 'DOI', 'Report Number', 'DOE Number', 'URL', ''])
-
-dc = '{http://purl.org/dc/elements/1.1/}'
-dcq = '{http://purl.org/dc/terms/}'
-
-for number in csv_f:
-	ostiId = number[0]
-	
-	results = requests.get('http://www.osti.gov/scitech/scitechxml?Identifier=' + ostiId)
-	tree = ElementTree.fromstring(results.content)
-	
-	for node in tree.iter():
+def getRecs(self):
+	for node in osti:
 		if node.tag == dc + 'title':
 			title = node.text
 			continue
@@ -50,6 +39,35 @@ for number in csv_f:
 			ostiUrl = node.text
 			continue
 	data.writerow([title, author, date, sub, description, ostiId, doi, reportNumber, doeNumber, ostiUrl])
+	#marcOut.write(newRec.as_marc())
 
-out.close()
+
+
+f = open('ostinos.csv')
+csv_f = csv.reader(f)
+
+out = open('osti_recs.csv', 'w')
+data = csv.writer(out)
+data.writerow(['Title', 'Author', 'Date', 'Subjects', 'Description', 'OstiID', 'DOI', 'Report Number', 'DOE Number', 'URL', ''])
+marcOut = open('ostimarc.mrc', 'w')
+
+dc = '{http://purl.org/dc/elements/1.1/}'
+dcq = '{http://purl.org/dc/terms/}'
+
+
+for number in csv_f:
+	ostiId = number[0]
+	newRec = Record()
 	
+	results = requests.get('http://www.osti.gov/scitech/scitechxml?Identifier=' + ostiId)
+	tree = etree.fromstring(results.content)
+	for node in tree.iter():
+		if node.tag == dc + 'ostiId':
+			if node.text == ostiId:
+				o = node.getparent()
+				osti = o.getchildren()
+				getRecs(osti)
+				continue
+		
+marcOut.close()	
+out.close()
