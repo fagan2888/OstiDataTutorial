7c59ba403624eaad5dab71d553d9e3a1566323d0
keknight <knight.kathryn@gmail.com>
2016-08-30 11:25:29 -0400

    updated ostinos.py

--- Python/ostinos.py
+++ Python/ostinos.py
@@ -38,17 +38,37 @@ def getRecs(self):
 			ostiUrl = node.text
 			continue
 	data.writerow([title, author, date, sub, description, ostiId, doi, reportNumber, doeNumber, ostiUrl])
-	#marcOut.write(newRec.as_marc())
+
+def getMarc(self):
+	
+	for node in osti:
+		if node.tag == dc + 'ostiId':
+			ostiNum = Field(tag = '086', indicators = [' ', ' '], subfields = ['a', node.text])
+			continue
+		if node.tag == dc + 'identifierReport':
+			repId = Field(tag = '088', indicators = [' ', ' '], subfields = ['a', node.text])
+			continue
+		if node.tag == dc + 'title':
+			title = Field(tag = '245', indicators = ['0', '0'], subfields=['a', node.text])
+			continue
+		if node.tag == dc + 'creator':
+			creator = Field(tag = '700', indicators = ['1', ' '], subfields = ['a', node.text,])
+			continue
+		
+	marc.add_field(ostiNum, repId, title, creator)
 
 
+#-------------------------------------------------
 
 f = open('ostinos.csv')
 csv_f = csv.reader(f)
 
-out = open('osti_recs.csv', 'w')
+out = open ('osti_recs.csv', 'w')
 data = csv.writer(out)
 data.writerow(['Title', 'Author', 'Date', 'Subjects', 'Description', 'OstiID', 'DOI', 'Report Number', 'DOE Number', 'URL', ''])
+
 marcOut = open('ostimarc.mrc', 'w')
+marc = Record()
 
 dc = '{http://purl.org/dc/elements/1.1/}'
 dcq = '{http://purl.org/dc/terms/}'
@@ -56,7 +76,6 @@ dcq = '{http://purl.org/dc/terms/}'
 
 for number in csv_f:
 	ostiId = number[0]
-	newRec = Record()
 	
 	results = requests.get('http://www.osti.gov/scitech/scitechxml?Identifier=' + ostiId)
 	tree = etree.fromstring(results.content)
@@ -66,7 +85,7 @@ for number in csv_f:
 				o = node.getparent()
 				osti = o.getchildren()
 				getRecs(osti)
+				getMarc(osti)
 				continue
-		
-marcOut.close()	
-out.close()
+
+marcOut.write(marc.as_marc())
